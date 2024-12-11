f5a429702cb7197ed4a7043fef0bef461184fad9
Oscar Ortiz <dev.oscarortiz@gmail.com>
2024-12-10 19:07:52 -0600

    🐛 Fix: resolve issue with conform.nvim

--- lua/nexide/core/conform.lua
+++ lua/nexide/core/conform.lua
@@ -1,35 +1,38 @@
 local M = {}
 
 M.setup = function()
-  local is_installed, conform = pcall(require, 'conform')
-  if not is_installed then return end
+	local is_installed, conform = pcall(require, "conform")
+	if not is_installed then
+		return
+	end
 
-  conform.setup({
-    formatters_by_fy = {
-      javascript = { 'prettier' },
-      typescript = { 'prettier' },
-      javascriptreact = { 'prettier' },
-      typescriptreact = { 'prettier' },
-      css = { 'prettier' },
-      html = { 'prettier' },
-      markdown = { 'prettier' },
-      json = { 'prettier' },
-      lua = { 'stylua' }
-    },
-    format_on_save = {
-      lsp_fallback = true,
-      async = false,
-      timeout_ms = 1000
-    }
-  })
+	conform.setup({
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			markdown = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		},
+	})
 
-  vim.keymap.set({ 'n', 'v' }, '<C-S-f>', function()
-    conform.format({
-      lsp_fallback = true,
-      async = false,
-      timeout_ms = 1000
-    })
-  end, { noremap = true, silent = true })
+	vim.keymap.set({ "n", "v" }, "<C-S-f>", function()
+		conform.format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		})
+	end, { noremap = true, silent = true })
 end
 
 return M
